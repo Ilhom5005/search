748c63bc7242ce92e21384285b610fad34792ccf
Ilhom Fayzulaev <ilhom5005@mail.ru>
2020-10-27 09:43:24 +0500

    update

--- pkg/search/search_test.go
+++ pkg/search/search_test.go
@@ -10,7 +10,7 @@ func TestAll_user(t *testing.T) {
 	ch := All(context.Background(), "Lorem", []string{"../../data/test.txt"})
 	results, ok := <-ch
 	if !ok {
-		t.Errorf("error: %v", ok)
+		t.Errorf("error: %v",ok)
 	}
-	log.Println("result: ", results)
+	log.Println("result: ",results)
 }
